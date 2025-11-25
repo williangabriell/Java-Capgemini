06b757e2241c965ab6924d81a05a3ce5bda1663d
Willian Gabriel <willianlopes20@hotmail.com>
2025-11-25 16:25:36 -0300

    first commit

---
+++ MeuPrimeiroProjeto/app/src/main/java/MeuPrimeiroProjeto/App.java
@@ -0,0 +1,12 @@
+package MeuPrimeiroProjeto;
+
+public class App {
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Olá Mundo");
+        System.out.println("Segundo código");
+    }
+}
