4d54f9a2a4da4223c425592161e6d737b0cd704f
Willian Gabriel <willianlopes20@hotmail.com>
2025-11-26 20:26:32 -0300

    EstruturaSwitch

---
+++ EstruturaSwitch/app/src/main/java/EstruturaSwitch/App.java
@@ -0,0 +1,25 @@
+package EstruturaSwitch;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        int codigoProduto = 2;
+        
+        switch(codigoProduto){
+            
+        case 1:   
+            
+            break;
+            
+        case 2:
+            
+            break;
+            
+        default:    
+            
+            
+        }
+        
+    }
+}
