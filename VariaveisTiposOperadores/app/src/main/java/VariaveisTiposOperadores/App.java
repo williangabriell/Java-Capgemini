2a7e21b31f9abd047fbf33681217d013a99d299f
Willian Gabriel <willianlopes20@hotmail.com>
2025-11-26 16:36:01 -0300

    VariaveisTiposOperadores

---
+++ VariaveisTiposOperadores/app/src/main/java/VariaveisTiposOperadores/App.java
@@ -0,0 +1,24 @@
+package VariaveisTiposOperadores;
+
+public class App {
+ 
+    public static void main(String[] args) {
+        
+        int idade = 0; // Números inteiro
+        float cotacaoDolar = 5.0f; // Números Váriáveis
+        double cotacaoEuro = 6.0d; //
+        char genero = 'M'; // Caractere único
+        byte placar; // 1 byte
+        boolean estaCadastrado = false; // Verdadeiro ou Falso
+        
+        
+        
+        String nome = "Esse é um texto de uma variável String"; // Variável primitiva para textos
+        
+      //  +  // Adição
+      //  -  // Subtração
+      //  /  // Divisão
+      //  *  // Multiplicação
+      //  %  // Resto da divisão
+    }
+}
