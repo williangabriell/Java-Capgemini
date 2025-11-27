522c6a0f2279a397bef9a5ce64c27ebd787d11b5
Willian Gabriel <willianlopes20@hotmail.com>
2025-11-27 19:23:48 -0300

    EstruturaFor

---
+++ EstruturaFor/app/src/main/java/EstruturaFor/App.java
@@ -0,0 +1,16 @@
+package EstruturaFor;
+
+public class App {
+
+
+    public static void main(String[] args) {
+        
+        int i = 0;
+       
+        for(; i <=10; i++){
+            System.out.println(i);
+        }
+        
+        
+    }
+}
