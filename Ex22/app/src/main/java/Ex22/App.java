281c8478718318a73138073485b788754cd9dc9c
Willian Gabriel <willianlopes20@hotmail.com>
2025-12-03 16:18:01 -0300

    Ex22 fix

--- Ex22/app/src/main/java/Ex22/App.java
+++ Ex22/app/src/main/java/Ex22/App.java
@@ -3,49 +3,54 @@ package Ex22;
 import java.util.Scanner;
 
 public class App {
+
     public static void main(String[] args) {
-        
+
         Scanner leitorScanner = new Scanner(System.in);
+
+        double totalCusto = 0;
+        double totalVenda = 0;
+        String nomeItem;
+
+        double precoCusto;
+        double precoVenda;
         
-        int quantProdutos = 40;
-        double contPrecoCusto = 0;
-        double contPrecoVenda = 0;
-        double media = 0;
-        
-        while(quantProdutos > 0) {
-            
-        System.out.println("Digite o nome do item: ");
-        String nomeItem = leitorScanner.nextLine();
-        
-        System.out.println("Digite o preço de custo do produto: ");
-        double precoCusto = leitorScanner.nextDouble();
-        
-        System.out.println("Digite o preço de venda do produto: ");
-        double precoVenda = leitorScanner.nextDouble();
-        
-        if (precoCusto < precoVenda) {
-            System.out.println("O item " + nomeItem + " deu Lucro");
-            System.out.println("Valor de custo de " + precoCusto + " R$");
-            System.out.println("Valor de vende de " + precoVenda + " R$");
-            
-        } else {
-            if(precoCusto == precoVenda) {
-                System.out.println("Esse item ficou empatado o valor");
+        int i = 0;
+        for (; i < 40; i++) {
+
+            System.out.println("Digite o nome do item: ");
+            nomeItem = leitorScanner.nextLine();
+
+            System.out.println("Digite o preço de custo do produto: ");
+            precoCusto = leitorScanner.nextDouble();
+
+            System.out.println("Digite o preço de venda do produto: ");
+            precoVenda = leitorScanner.nextDouble();
+
+            totalCusto = totalCusto + precoCusto;
+            totalVenda = totalVenda + precoVenda;
+
+            if (precoCusto < precoVenda) {
+                System.out.println("O item " + nomeItem + " deu Lucro");
                 System.out.println("Valor de custo de " + precoCusto + " R$");
                 System.out.println("Valor de vende de " + precoVenda + " R$");
+
             } else {
-                System.out.println("Esse item deu prejuizo");
-                System.out.println("Valor de custo de " + precoCusto + " R$");
-                System.out.println("Valor de vende de " + precoVenda + " R$");
+                if (precoCusto == precoVenda) {
+                    System.out.println("Esse item ficou empatado o valor");
+                    System.out.println("Valor de custo de " + precoCusto + " R$");
+                    System.out.println("Valor de vende de " + precoVenda + " R$");
+                } else {
+                    System.out.println("Esse item deu prejuizo");
+                    System.out.println("Valor de custo de " + precoCusto + " R$");
+                    System.out.println("Valor de vende de " + precoVenda + " R$");
+                }
+
             }
-            quantProdutos--;  
-            contPrecoCusto = contPrecoCusto + precoCusto;
-            contPrecoVenda = contPrecoVenda + precoVenda;
         }
-       }
-        media = (contPrecoCusto + contPrecoVenda) / quantProdutos;
-        System.out.print("Media do preço de custo e do preço de venda: " + media);
-        
-        
+
+        System.out.println("Media do preço de custo: " + (totalCusto/ i) );
+        System.out.println("Media do preço de venda: " + (totalVenda/ i) );
+
     }
 }
